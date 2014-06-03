4492da3d3ccab4c8a8448babf2d7bff1f65f6342
Paolo Bernasconi <paolo.enrico.bernasconi@gmail.com>
2014-06-03 19:08:37 +0200

    initial commit

---
+++ www/js/controllers.js
@@ -0,0 +1,15 @@
+angular.module('starter.controllers', [])
+
+  .controller('MenuCtrl', function ($scope) {
+    console.log('menu controller');
+  })
+
+  .controller('SubMenuCtrl', function ($scope) {
+    console.log('sub menu controller');
+
+  })
+
+  .controller('MainContentCtrl', function ($scope) {
+    console.log('main content controller');
+
+  });
